f979af2d02763f220778f0d2de93dc2acc9acd4f
struct78 <david@struct.com.au>
2018-12-07 09:01:20 +1100

    Add build script to create .env.production

---
+++ app/build.js
@@ -0,0 +1,10 @@
+const fs = require('fs')
+fs.writeFileSync('./.env.production', `
+GOOGLE_API_KEY=${process.env.GOOGLE_API_KEY}
+API_URL_ARRIVALS=${process.env.API_URL_ARRIVALS}
+API_URL_DEPARTURES=${process.env.API_URL_DEPARTURES}
+REQUEST_INTERVAL=${process.env.REQUEST_INTERVAL}
+TEMPO=${process.env.TEMPO}
+BEATS_PER_BAR=${process.env.BEATS_PER_BAR}
+BEAT_VALUE=${process.env.BEAT_VALUE}
+`);
