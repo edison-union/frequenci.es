0092818b8df3771f69e5a725fed76761bb85ec69
struct78 <david@struct.com.au>
2018-12-28 15:28:05 +1100

    Trim MP3s

--- app/static/sounds/convert.sh
+++ app/static/sounds/convert.sh
@@ -1,6 +1,6 @@
 for i in *.wav;
   do name=`echo $i | cut -d'.' -f1`;
   echo $name;
-  ffmpeg -i "$i" -vn -ar 44100 -ac 2 -ab 192k -f mp3 ${name}.mp3
-  
+  ffmpeg -i "$i" -vn -ar 44100 -ac 2 -ab 192k -f mp3 -y ${name}.mp3
+
 done
